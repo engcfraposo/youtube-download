4bc1b732f080f1f9bbb8c4d9ba1912c015646672
Cláudio Rapôso <engcfraposo@gmail.com>
2022-08-17 10:41:46 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import ytdl from "ytdl-core";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/healthcheck", (req, res) => {
